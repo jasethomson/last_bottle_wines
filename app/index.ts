899944fc8a3ec5f3da18b84cb4dc6ea6320edc7b
Jase Thomson <jasethomson31@gmail.com>
2025-01-21 09:33:14 -0800

    set up husky, eslint and prettier

--- app/index.ts
+++ app/index.ts
@@ -1,13 +1,10 @@
 import { config } from 'dotenv';
 config();
 
-import { checkForLatestOffer, logTool } from './utils';
+import { checkForLatestOffer } from './utils';
 
 const app = async () => {
   const currOffer = await checkForLatestOffer({ currOffer: null });
-  const testVar = 'hello';
-  testVar = "don't do this";
-  logTool({ msg: [testVar] });
   setInterval(async () => await checkForLatestOffer({ currOffer }), parseInt(process.env.REQUEST_FREQUENCY_IN_MS));
 };
 
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,35 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
-  {languageOptions: { globals: globals.browser }},
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
+  // ...tseslint.configs.recommended,
+  {
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'no-duplicate-imports': 'error',
+      camelcase: 'error',
+      quotes: ['error', 'single', { avoidEscape: true }],
+      'no-multiple-empty-lines': ['error', { max: 1 }],
+      'no-console': 'error',
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
+    },
+  },
 ];
