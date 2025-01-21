9cad43ae9253d1e28249605bcc7b69aa38d01c86
Jase Thomson <jasethomson31@gmail.com>
2025-01-21 09:02:12 -0800

    Testing pre-commit hook

--- app/index.ts
+++ app/index.ts
@@ -1,11 +1,13 @@
 import { config } from 'dotenv';
 config();
 
-import { checkForLatestOffer } from './utils';
+import { checkForLatestOffer, logTool } from './utils';
 
 const app = async () => {
   const currOffer = await checkForLatestOffer({ currOffer: null });
-
+  const testVar = 'hello';
+  testVar = "don't do this";
+  logTool({ msg: [testVar] });
   setInterval(async () => await checkForLatestOffer({ currOffer }), parseInt(process.env.REQUEST_FREQUENCY_IN_MS));
 };
 
